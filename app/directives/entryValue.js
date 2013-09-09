dd36855ba07eb1a1e6c811a9fd149e003984a7b8
Alexey Bykov <jusioa@gmail.com>
2013-09-10 01:19:37 +0200

    feature - added basic coloring to the values in table

--- app/directives/entryValue.js
+++ app/directives/entryValue.js
@@ -1,9 +1,69 @@
-angular.module("storageExplorer").directive("entryValue", function () {
+angular.module("storageExplorer").directive("entryValue", function ($compile) {
     function checkWidth(string) {
         if (string.length > 50) {
             return string.substr(0, 49) + "...";
         }
         return string;
+    }
+
+    var stringTemplate = '<span class="quote">"</span><span class="string">{{displayedValue}}</span><span class="quote">"</span>';
+    var booleanTemplate = '<span class="boolean">{{displayedValue}}</span>';
+    var numberTemplate = '<span class="number">{{displayedValue}}</span>';
+
+    function escape(input) {
+        var string = '' + input;
+        return string.replace('<', '&lt;').replace('>', '&gt;')
+    }
+
+    function string(value) {
+        return stringTemplate.replace('{{displayedValue}}', checkWidth(escape(value)));
+    }
+
+    function bool(value) {
+        return booleanTemplate.replace('{{displayedValue}}', value);
+    }
+
+    function num(value) {
+        return numberTemplate.replace('{{displayedValue}}', value)
+    }
+
+    function buildObjectPreview(obj) {
+
+        function rval(val) {
+            if (angular.isString(val)) {
+                return string(val);
+            }
+            if (angular.isNumber(val)) {
+                return num(val);
+            }
+            if (angular.isObject(val)) {
+                return buildObjectPreview(val);
+            }
+            if (val === null) {
+                return bool('null');
+            }
+            return bool(val);
+        }
+
+
+        var element = angular.element('<span></span>');
+        if (angular.isArray(obj)) {
+            element.append(angular.element('<span>[</span>'));
+            element.append(angular.element('<span>array</span>'));
+            element.append(angular.element('<span> ]</span>'))
+        } else if (angular.isObject(obj)) {
+            element.append(angular.element('<span>{</span>'));
+            angular.forEach(obj, function (value, key) {
+                element.append(string(key)).append(':').append(rval(value)).append(',');
+            });
+
+
+            element.append(angular.element('<span>}</span>'))
+        }
+        element.bind("click", function () {
+
+        });
+        return element;
     }
 
     return {
@@ -13,25 +73,18 @@ angular.module("storageExplorer").directive("entryValue", function () {
         },
         link: function (scope, element, attr) {
             var value = scope.value;
-            element.addClass("displayedValue");
             if (angular.isString(value)) {
-                element.html('"' + checkWidth(value) + '"');
-                return;
+                element.html(string(value));
+            } else if (angular.isNumber(value)) {
+                element.html(num(value));
+            } else if (angular.isObject(value)) {
+                element.html('');
+                element.append(checkWidth(angular.toJson(value)));
+            } else if (value === null) {
+                element.html(bool('null'));
+            } else {
+                element.html(bool(value));
             }
-            if (angular.isNumber(value)) {
-                element.html(value);
-                return;
-            }
-            if (angular.isObject(value)) {
-                element.html(checkWidth(angular.toJson(value)));
-                return;
-            }
-            if (value === null) {
-                element.html("null");
-                return;
-            }
-            element.html(value);
-
         }
     }
 
