82123188b077d2faace87a500b58e5b07fab273d
Alexey Bykov <jusioa@gmail.com>
2013-09-12 00:04:00 +0200

    bugfix - allow to use explorer on extensions

--- app/chrome/devtools.js
+++ app/chrome/devtools.js
@@ -1,14 +1,11 @@
 var panels = chrome.devtools.panels;
-if (!chrome.devtools.inspectedWindow.tabId) {
 
-    chrome.devtools.inspectedWindow.eval("!!chrome.storage", function (res) {
-        if (res) {
-            panels.create(
-                "Storage Explorer",
-                "img/angular.png",
-                "/app/html/panel.html"
-            );
-        }
-    });
-
-}
+chrome.devtools.inspectedWindow.eval("!!chrome.storage", function (res) {
+    if (res) {
+        panels.create(
+            "Storage Explorer",
+            "img/angular.png",
+            "/app/html/panel.html"
+        );
+    }
+});
