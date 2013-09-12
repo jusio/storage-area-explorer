37a209945d220c1ebeb2d30935f809b139c77684
Alexey Bykov <jusioa@gmail.com>
2013-09-12 20:32:29 +0200

    fixing build

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,17 +4,31 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         clean: ['build'],
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js'
+            }
+        },
         zip: {
             release: {
+                compression: 'DEFLATE',
                 src: [
-                    'app/css/*.css',
+                    'app/css/styles.css',
+                    'app/css/bootstrap/css/bootstrap.css',
+                    'app/css/bootstrap/js/bootstrap.js',
+                    'app/css/bootstrap/fonts/glyphicons-halflings-regular.woff',
                     'app/*.js',
-                    'app/components/*/*.js',
+                    'app/components/jquery/jquery.js',
+                    'app/components/angular/angular.js',
                     'app/chrome/*.js',
                     'app/html/*.html',
+                    'app/services/*.js',
+                    'app/directives/*.js',
+                    'app/controllers/*.js',
+                    'app/filters/*.js',
                     'manifest.json'
                 ],
-                dest: 'build/storage-area-explorer-release-' + Date.now() + '.zip'
+                dest: 'build/storage-area-explorer-v<%=pkg.version%>_' + Date.now() + '.zip'
             }
 
         }
@@ -22,9 +36,9 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-zip');
-
+    grunt.loadNpmTasks('grunt-karma');
 
     // Default task(s).
-    grunt.registerTask('default', ['clean', 'zip']);
+    grunt.registerTask('default', ['clean', 'karma', 'zip']);
 
 };
