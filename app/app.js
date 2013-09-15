bc9ab6259f808647837cd9b69606db5ed77757a1
Alexey Bykov <jusioa@gmail.com>
2013-09-16 01:03:01 +0200

    bug - production  html didn't contain all resources

--- app/app.js
+++ app/app.js
@@ -3,10 +3,3 @@ angular.module("storageExplorer", []).
     .value("runtime", chrome.runtime)
     .value("devtools", chrome.devtools);
 
-
-function dummyLog(message) {
-
-//    var elementById = document.getElementById("display");
-//    elementById.innerHTML += "<br>";
-//    elementById.innerText += message;
-}
