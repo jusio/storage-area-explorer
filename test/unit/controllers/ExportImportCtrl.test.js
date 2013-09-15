b9edb4606a3e236477b9d01eea3602ec8c132a66
Alexey Bykov <jusioa@gmail.com>
2013-09-15 02:29:04 +0200

    test - finished covering controllers with tests

--- test/unit/controllers/ExportImportCtrl.test.js
+++ test/unit/controllers/ExportImportCtrl.test.js
@@ -3,50 +3,36 @@ describe("Testing ExportImport controller", function () {
     var localStorageMock;
     var rootScope;
     var windowMock;
+    var q;
 
     var ctrl, scope;
     beforeEach(module("storageExplorer"));
 
     beforeEach(function () {
-        var methods = ['get', 'set', 'clear', 'remove', 'getMeta'];
+        storageExplorer.mocks.reset();
+        var methods = ['set', 'clear','get'];
         localStorageMock = jasmine.createSpyObj("storage.local", methods);
-        localStorageMock.get.andCallFake(function (callback) {
-            callback({test: "test"})
-        });
-        var getBytesInUse = jasmine.createSpy("getBytesInUse").andCallFake(function (firstParam, secondParam) {
-            var callback;
-            if (angular.isFunction(firstParam)) {
-                callback = firstParam;
-            } else {
-                callback = secondParam;
-            }
-
-            callback(100);
-        });
-
-        localStorageMock.getBytesInUse = getBytesInUse;
+        localStorageMock.clear = storageExplorer.mocks.asyncMethodStub();
         storageMock = {
             local: localStorageMock
         };
-        prettyJsonMock = jasmine.createSpy("prettyJson");
-        appContextMock = jasmine.createSpy("appContext").andCallFake(function () {
-            return {appId: "id", name: "name"};
-        });
+        prettyJsonMock = jasmine.createSpy("prettyJson").andReturn("{}");
+        appContextMock = jasmine.createSpy("appContext").andReturn("");
         clipboardMock = {};
         clipboardMock.put = jasmine.createSpy("put");
         clipboardMock.get = jasmine.createSpy("get");
         windowMock = {
-            confirm: jasmine.createSpy("confirm").andCallFake(function () {
-                return false;
-            })
+            confirm: jasmine.createSpy("confirm").andReturn(false)
         };
         fileSystemMock = {
-            promptFileDownload:function(){},
-            promptFileSelectionAsText:function(){}
+            promptFileDownload: jasmine.createSpy("promptFileDownload"),
+            promptFileSelectionAsText: function () {
+            }
         }
     });
 
-    beforeEach(inject(function ($rootScope, $controller) {
+    beforeEach(inject(function ($rootScope, $controller, $q) {
+        q = $q;
         scope = $rootScope.$new();
         rootScope = $rootScope;
         rootScope.currentType = "local";
@@ -58,7 +44,73 @@ describe("Testing ExportImport controller", function () {
                 $window: windowMock,
                 fileSystem: fileSystemMock
             }
-        )
+        );
+
     }));
+
+    it("should expose import export importClipboard exportClipboard", function () {
+        expect(scope.import).toBeDefined();
+        expect(scope.export).toBeDefined();
+        expect(scope.importClipboard).toBeDefined();
+        expect(scope.exportClipboard).toBeDefined();
+    });
+
+    it("on importClipboard call it should call clipboard.get, then storage.clear and storage.set", function () {
+        rootScope.currentType = "local";
+        clipboardMock.get.andReturn(q.when('{}'));
+        scope.importClipboard();
+        rootScope.$apply();
+        expect(clipboardMock.get).toHaveBeenCalled();
+        expect(localStorageMock.clear).toHaveBeenCalled();
+        expect(localStorageMock.set).toHaveBeenCalled();
+    });
+
+    it(" should set error when parsing failed on importClipboard", function () {
+        rootScope.currentType = "local";
+        clipboardMock.get.andReturn(q.when('this_will_cause_error'));
+
+        scope.importClipboard();
+        rootScope.$apply();
+        expect(rootScope.importError).toBeDefined();
+    });
+
+    it(" should filesystem promptFileSelectionAsText ,storage.clear storage.set on scope.import ", function () {
+        spyOn(fileSystemMock, 'promptFileSelectionAsText').andReturn(q.when("{}"));
+        scope.import();
+        rootScope.$apply();
+        expect(fileSystemMock.promptFileSelectionAsText).toHaveBeenCalled();
+        expect(localStorageMock.clear).toHaveBeenCalled();
+        expect(localStorageMock.set).toHaveBeenCalled();
+    });
+
+    it(" should set importError when wrong file format have been provided on scope.import",function(){
+        rootScope.importError = null;
+        spyOn(fileSystemMock, 'promptFileSelectionAsText').andReturn(q.when("{"));
+        scope.import();
+        rootScope.$apply();
+        expect(rootScope.importError).toBeDefined();
+    });
+
+
+    it("should call storage.get and clipboard.put on scope.exportClipboard",function(){
+        rootScope.currentType = "local";
+        localStorageMock.get = storageExplorer.mocks.asyncMethodStub({});
+        scope.exportClipboard();
+        rootScope.$apply();
+        expect(localStorageMock.get).toHaveBeenCalled();
+        expect(clipboardMock.put).toHaveBeenCalled();
+    });
+
+    it(" should call storage.get, filesystemPromptFileDownload on scope.export",function(){
+        appContextMock.andReturn(q.when({name:"name"}));
+        localStorageMock.get = storageExplorer.mocks.asyncMethodStub({});
+        rootScope.currentType = "local";
+        scope.export();
+        rootScope.$apply();
+
+        expect(appContextMock).toHaveBeenCalled();
+        expect(localStorageMock.get).toHaveBeenCalled();
+        expect(fileSystemMock.promptFileDownload).toHaveBeenCalled();
+    });
 
 });
