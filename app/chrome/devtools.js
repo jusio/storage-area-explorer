e098af972e3b0e09bf1c52f8d6af7f15b2541ae8
Alexey Bykov <jusioa@gmail.com>
2013-09-18 21:36:48 +0200

    chore - removing alerts

--- app/chrome/devtools.js
+++ app/chrome/devtools.js
@@ -3,7 +3,6 @@ function initializeDevtoolsPage(panels, inspectedWindow) {
 
     inspectedWindow.eval("!!chrome.runtime && chrome.runtime.getManifest()", function (manifest) {
         if (!!manifest.permissions && manifest.permissions.indexOf("storage") > -1) {
-            alert("Creating page");
             panels.create(
                 "Storage Explorer",
                 "img/angular.png",
